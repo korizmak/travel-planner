937db32a0845b768894daec0d8a54c1a51e5a014
Katarina Korizma <katarina.korizma@gmail.com>
2026-07-01 16:13:55 +0200

    Create data Models (Trip, TravelEvent, CostItem, EventType)

---
+++ src/app/models/event-type.enum.ts
@@ -0,0 +1,8 @@
+export enum EventType {
+  ACCOMMODATION = 'accommodation',
+  TRANSPORT = 'transport',
+  SIGHTSEEING = 'sightseeing',
+  FOOD = 'food',
+  ENTERTAINMENT = 'entertainment',
+  OTHER = 'other'
+}
