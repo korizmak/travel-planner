bfa1c90553f62182026e08f6324ed5935ebacb03
Katarina Korizma <katarina.korizma@gmail.com>
2026-07-02 15:12:21 +0200

    Fix import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideIonicAngular } from '@ionic/angular';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app.routes';
 
